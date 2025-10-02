18fdfbc53de8a39e4e67185315b3171bedf9160a
isseu181 <isseugueye219@gmail.com>
2025-10-02 13:45:37 +0000

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -1,3 +1,4 @@
+i
 # ================================
 # deployment.py - Déploiement Random Forest
 # ================================
@@ -122,4 +123,5 @@ def show_deployment():
             st.success(f"✅ Évolution prévue : **Favorable** (Probabilité de complication : {pred_proba:.2f})")
         else:
             st.error(f"⚠️ Évolution prévue : **Complications attendues** (Probabilité : {pred_proba:.2f})")
+
 
