1e00de6e479c17f0104215d0b367def1e5cf261a
isseu181 <isseugueye219@gmail.com>
2025-10-04 18:50:41 +0000

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,4 +1,4 @@
-i# eda.py
+# eda.py
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -93,4 +93,5 @@ def show_eda():
                     # Variable quantitative
                     fig = px.histogram(df_seg, x=var_choisie, nbins=20, title=f"Distribution de {var_choisie}")
                     st.plotly_chart(fig, use_container_width=True)
+
 
