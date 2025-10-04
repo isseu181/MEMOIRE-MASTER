8b2f43f12f4daa4fe624fdc2f17046c9aae9d9fe
isseu181 <isseugueye219@gmail.com>
2025-10-04 20:02:57 +0000

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,4 +1,4 @@
-i# eda.py
+# eda.py
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -214,4 +214,5 @@ def show_eda():
         
         if bio_data:
             st.table(pd.DataFrame(bio_data).T.round(2))
+
 
