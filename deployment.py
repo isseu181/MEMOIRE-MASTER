6f2439b5edb93454b51be11b1cf6abf93434b310
isseu181 <isseugueye219@gmail.com>
2025-10-10 20:53:46 +0300

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -1,4 +1,4 @@
-i# ================================
+# ================================
 # deployment.py - Déploiement Random Forest 
 # ================================
 import streamlit as st
@@ -210,3 +210,4 @@ def show_deployment():
                 </ul>
             </div>
             """, unsafe_allow_html=True)
+
